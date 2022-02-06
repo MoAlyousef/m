29714b4938e2ccce6a1fc7d48ff717908cdaf51f
Mohammed Alyousef <may642_2000@hotmail.com>
2022-02-06 13:05:58 +0300

    first commit

---
+++ src/literals.h
@@ -0,0 +1,151 @@
+static const char *my_resume = R"lit(
+<h2>Resumé</h2>
+
+<hr>
+
+
+<div>
+    <h5>
+        Assistant professor and consultant neurosurgeon
+    </h5>
+    <p>
+        at King Abdulaziz University Hospital,
+        Jeddah, Saudi Arabia
+    </p>
+    <br>
+    <h6>
+        Experience
+    </h6>
+    <ul>
+        <li>
+            <h6>
+                King AbdulAziz University
+            </h6>
+            <p>
+                Assistant Professor, Consultant Neurosurgeon
+                <br>
+                November 2016 - Present (3 years 6 months)
+                Jeddah, Saudi Arabia
+            <p>
+        </li>
+        <li>
+            <h6>
+                Hôpital Erasme
+            </h6>
+            <p>
+                Skull base and vascular neurosurgery fellow
+                <br>
+                October 2017 October 2018 (1 year)
+                Brussels, Belgium
+            <p>
+        </li>
+        <li>
+            <h6>
+                APHP
+            </h6>
+            <p>
+                Neurosurgery resident
+                <br>
+                November 2011 November 2016 (5 years 1 month)
+                Paris, France
+            <p>
+        </li>
+        <li>
+            <h6>
+                King AbdulAziz University
+            </h6>
+            <p>
+                Neurosurgery resident/teaching assistant
+                <br>
+                September 2008 - September 2010 (2 years 1 month)
+                Jeddah, Saudi Arabia
+            <p>
+        </li>
+    </ul>
+
+    <h6>
+        Education
+    </h6>
+    <ul>
+        <li>
+            <h6>
+                Université libre de Bruxelles
+            </h6>
+            <p>
+                Clinical fellowship, Skull base surgery and vascular neurosurgery
+                <br>
+                (2017 - 2018)
+                Brussels, Belgium
+            <p>
+        </li>
+        <li>
+            <h6>
+                Université René Descartes (Paris V)
+            </h6>
+            <p>
+                DES (Diplôme d'études spécialisées), Neurosurgery
+                <br>
+                (2011 - 2016)
+                Paris, France
+            <p>
+        </li>
+        <li>
+            <h6>
+                King Abdulaziz University
+            </h6>
+            <p>
+                Bachelor of Medicine, Bachelor of Surgery (MBBS), Medicine
+                <br>
+                (2001 - 2007)
+                Jeddah, Saudi Arabia
+            <p>
+        </li>
+    </ul>
+</div>
+)lit";
+
+static const char *about_me = R"lit(
+<h3>Mohammed Alyousef</h3>
+<p>
+    I'm a neurosurgeon at King Abdulaziz University Hospital, who's interested in programming,
+    linguistics and history.
+    I mainly develop using C, C++ and Rust. I contribute to open source software.
+    I'm also interested in linguistics, especially in comparative Semitic linguistics. I'm a
+    language aficionado as well.
+    I speak Arabic, English and French. Fields of history that interest me include ancient history
+    of the Middle East.
+    I also enjoy traveling and photography. I've lived in several countries:
+<ul>
+    <li>
+        Jeddah, Saudi Arabia
+    </li>
+    <li>
+        Manchester, UK
+    </li>
+    <li>
+        Chicago, US
+    </li>
+    <li>
+        Paris, France
+    </li>
+    <li>
+        Brussels, Belgium
+    </li>
+</ul>
+</p>
+</div>
+<div>
+    <img src="/assets/image.jpg" alt="Mohammed Alyousef" width="250" height="250">
+</div>
+<div>
+    <h3>Details</h3>
+    <p>
+        <strong>Name:</strong><br>
+        Mohammed Alyousef<br>
+        <strong>Work:</strong><br>
+        King Abdulaziz University
+    </p>
+    <strong>Location:</strong><br>
+    Jeddah, Saudi Arabia </p>
+</div>
+)lit";
---
+++ src/main.cpp
@@ -0,0 +1,193 @@
+#include <livid/livid.hpp>
+#include <string>
+
+#include "literals.h"
+
+using namespace livid;
+
+using Nav = Widget<WidgetType::Nav>;
+using Div = Widget<WidgetType::Div>;
+using A = Widget<WidgetType::A>;
+using Span = Widget<WidgetType::Span>;
+using Img = Widget<WidgetType::Img>;
+using H1 = Widget<WidgetType::H1>;
+using P = Widget<WidgetType::P>;
+using Article = Widget<WidgetType::Article>;
+using Li = Widget<WidgetType::Li>;
+using Ul = Widget<WidgetType::Ul>;
+
+void expand() {
+    auto nav_menu = WidgetBase::from_id("navmenu");
+    auto is_active = nav_menu.klass().find("is-active") != std::string::npos;
+    if (is_active) {
+        nav_menu.klass("navbar-menu");
+    } else {
+        nav_menu.klass("navbar-menu is-active");
+    }
+}
+
+// clang-format off
+void about() {
+    auto main_div = WidgetBase::from_id("maindiv");
+    main_div.inner_html("");
+    main_div.append(
+        Article().klass("message is-info").append(
+            Div().klass("message-header").append(
+                P().text("About")
+            )
+        ).append(
+            Div().klass("message-body")
+                .inner_html("This site was created using <a href='https://github.com/MoAlyousef/livid'>Livid</a>, a C++ wasm frontend library and uses Bulma and font-awesome for the CSS.")
+        )
+    );
+}
+
+void contact() {
+    auto main_div = WidgetBase::from_id("maindiv");
+    main_div.inner_html("");
+    main_div.append(
+        Div().klass("card").append(
+            Div().klass("card-content").append(
+                H1().inner_html("Contact")
+            ).append(
+                P().inner_html("I can be contacted using the following media:")
+            ).append(
+                Ul().append(
+                    Li().append(
+                        A().href("https://twitter.com/MoAlyousef")
+                            .inner_html("<span class='fa fa-twitter'></span>    http://twitter.com/MoAlyousef")
+                    )
+                ).append(
+                    Li().append(
+                        A().href("https://sa.linkedin.com/in/moalyousef")
+                            .inner_html("<span class='fa fa-linkedin-square'></span>    https://sa.linkedin.com/in/moalyousef")
+                    )
+                ).append(
+                    Li().append(
+                        A().href("https://github.com/MoAlyousef")
+                            .inner_html("<span class='fa fa-github'></span>    https://github.com/MoAlyousef")
+                    )
+                ).append(
+                    Li().append(
+                        A().href("mailto:mohammed.alyousef@neurosrg.com")
+                            .inner_html("<span class='fa fa-envelope'></span><span style='unicode-bidi:bidi-override; direction: rtl;'>moc.grsoruen@fesuoyla.demmahom</span>")
+                    )
+                ).append(
+                    Li().append(
+                        A().href("mailto:maalyousef@kau.edu.sa")
+                            .inner_html("<span class='fa fa-envelope'></span><span style='unicode-bidi:bidi-override; direction: rtl;'>as.ude.uak@fesuoylaam</span>")
+                    )
+                )
+            )
+        )
+    );
+}
+
+void resume() {
+    auto main_div = WidgetBase::from_id("maindiv");
+    main_div.inner_html("");
+    main_div.append(
+        Div().klass("card").append(
+            Div().klass("card-content").append(
+                Div().inner_html(my_resume)
+            )
+        )
+    );
+}
+
+void projs() {
+    auto main_div = WidgetBase::from_id("maindiv");
+    main_div.inner_html("");
+    main_div.append(
+        Div().klass("card").append(
+            Div().klass("card-content").append(
+                H1().inner_html("Projects")
+            ).append(
+                P().inner_html("Some of my projects:")
+            ).append(
+                Ul().append(
+                    Li().append(
+                        A().href("https://github.com/fltk-rs/fltk-rs")
+                            .inner_html("<span class='fa fa-github'></span>    https://github.com/fltk-rs/fltk-rs")
+                    ).append(
+                        P().text("Rust bindings for the FLTK gui library.")
+                    )
+                ).append(
+                    Li().append(
+                        A().href("https://github.com/build-cpp/cmkr")
+                            .inner_html("<span class='fa fa-github'></span>    https://github.com/build-cpp/cmkr")
+                    ).append(
+                        P().text("A CMakeLists.txt generator from TOML.")
+                    )
+                ).append(
+                    Li().append(
+                        A().href("https://github.com/MoAlyousef/cfltk")
+                            .inner_html("<span class='fa fa-github'></span>    https://github.com/MoAlyousef/cfltk")
+                    ).append(
+                        P().text("A C89 wrapper for FLTK.")
+                    )
+                ).append(
+                    Li().append(
+                        A().href("https://github.com/MoAlyousef/soloud-rs")
+                            .inner_html("<span class='fa fa-github'></span>    https://github.com/MoAlyousef/soloud-rs")
+                    ).append(
+                        P().text("Rust bindings for the Soloud audio playback/synth library.")
+                    )
+                ).append(
+                    Li().append(
+                        A().href("https://github.com/MoAlyousef/livid")
+                            .inner_html("<span class='fa fa-github'></span>    https://github.com/MoAlyousef/livid")
+                    ).append(
+                        P().text("A single header C++ wasm frontend library leveraging Emscripten.")
+                    )
+                )
+            )
+        )
+    );
+}
+
+void create_navbar() {
+    Nav().klass("navbar bd-navbar").append(
+        Div().klass("navbar-brand").append(
+            A().klass("navbar-item").href("/").append(
+                Img().attr("src", "/assets/brand.png")
+            )
+        ).append(
+            A().klass("navbar-burger").append(
+                Span()
+            ).append(
+                Span()
+            ).append(
+                Span()
+            ).handle(Event::Click, expand).attr("data-target", "navmenu")
+        )
+    ).append(
+        Div().id("navmenu").klass("navbar-menu").append(
+            Div().klass("navbar-start").append(
+                A().klass("navbar-item").href("/").text("Home")
+            ).append(
+                A().klass("navbar-item").text("Projects").handle(Event::Click, projs)
+            ).append(
+                A().klass("navbar-item").text("Resume").handle(Event::Click, resume)
+            ).append(
+                A().klass("navbar-item").text("Contact").handle(Event::Click, contact)
+            ).append(
+                A().klass("navbar-item").text("About").handle(Event::Click, about)
+            )
+        ).append(
+            Div().klass("navbar-end")
+        )
+    );
+}
+// clang-format on
+
+int main() {
+    create_navbar();
+    Div().klass("content is-centered").id("maindiv").append(
+        Div().klass("card").append(
+            Div().klass("card-content").append(
+                Div().inner_html(about_me)
+            )
+        )
+    );
+}
